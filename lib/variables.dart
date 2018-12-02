9cef5b0c2218d0c308623d6ad61cdb807fddca79
Kristopher Andrews <kristopherca@gmail.com>
2018-12-02 16:24:02 -0500

    Removed API Key

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-final apiKey = "d1a35156853b37f2a18d6be5f932094a";
+final apiKey = "???????????";
 final defaultCity = "Fort Myers";
 var searchController = TextEditingController();
 var searchText = "";
